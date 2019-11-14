5e4f7f4f6710ab6185d6755291fb5863ddde0aab
kl <kl@debian>
2019-11-14 12:08:36 +0800

    initial init

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Hello = () => {
+  return <div>What's Up?</div>;
+};
+
+ReactDOM.render(<Hello />, document.getElementById("app"));
